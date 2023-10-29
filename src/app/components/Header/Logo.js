44c5cf9f834a5eee5b86e8094a93bba67f6789c1
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-10-29 18:54:01 +0200

    started adding blog entries and using the documentation for Contently

---
+++ src/app/components/Header/Logo.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import profileImg from "@/project-files/profile-img.png"
+
+const Logo = () => {
+  return (
+    <Link href="/" className='flex items-center text-dark'>
+        <div className='w-16 rounded-full overflow-hidden border border-solid border-dark mr-4'>
+            <Image src={profileImg} alt="Codebucks" className='w-full h-auto rounded-full'></Image>
+        </div>
+        <span className='font-bold text-xl'>Codebucks </span>
+    </Link>
+  )
+}
+
+export default Logo
