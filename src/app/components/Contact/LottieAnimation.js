6cbc94f575363900add46a039f0beb22025a9881
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-11-04 13:53:58 +0200

    created contact page. Used React hook forms and lottie animation

--- src/app/components/Contact/LottieAnimation.js
+++ src/app/components/Contact/LottieAnimation.js
@@ -1,18 +1,17 @@
+"use client"
+
 import React from 'react';
-import { DotLottiePlayer, Controls } from '@dotlottie/react-player';
+import { DotLottiePlayer } from '@dotlottie/react-player';
 import '@dotlottie/react-player/dist/index.css';
 
 const LottieAnimation = () => {
   return (
-    <div>
       <DotLottiePlayer
-        src="/path-to-lottie.lottie"
+        src="/lottie-animation.lottie"
         autoplay
         loop
       >
-        <Controls />
       </DotLottiePlayer>
-    </div>
   );
 };
 
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,14 +1,18 @@
+import ContactForm from "../components/Contact/ContactForm";
+import LottieAnimation from "../components/Contact/LottieAnimation";
 
 
 
 export default function Contact() {
 
     return <section className="w-full h-[75vh] border-b-2 border-solid border-dark flex flex-row items-center justify-center text-dark"> 
-        <div>
-            lottie animation
+        <div className="inline-block w-2/5 h-full border-r-2 border-solid border-dark">
+            <LottieAnimation />
         </div>
-        <div>
-            contact form
+        <div className="w-3/5 flex flex-col items-start justify-center px-16 pb-8">
+            <h2 className="font-bold capitalize text-4xl">Let's Connect!</h2>
+            <ContactForm />
+            
         </div>
     </section>
 
