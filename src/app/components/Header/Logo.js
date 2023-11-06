9ae270e311b4978f4044861821ff0a9eeb278e94
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-11-06 07:37:06 +0200

    added dark/light mode and began adding responsiveness

--- src/app/components/Header/Logo.js
+++ src/app/components/Header/Logo.js
@@ -4,11 +4,11 @@ import profileImg from "@/project-files/profile-img.png"
 
 const Logo = () => {
   return (
-    <Link href="/" className='flex items-center text-dark'>
-        <div className='w-16 rounded-full overflow-hidden border border-solid border-dark mr-4'>
+    <Link href="/" className='flex items-center text-dark dark:text-light'>
+        <div className='md:w-16 w-12 rounded-full overflow-hidden border border-solid border-dark dark:border-light md:mr-4 mr-2'>
             <Image src={profileImg} alt="Codebucks" className='w-full h-auto rounded-full'></Image>
         </div>
-        <span className='font-bold text-xl'>Codebucks </span>
+        <span className='font-bold dark:font-semibold md:text-xl text-lg'>Codebucks </span>
     </Link>
   )
 }
--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -1,33 +1,98 @@
-"use client"
+"use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import Link from "next/link";
-import { DribbbleIcon, GithubIcon, LinkedinIcon, SunIcon, TwitterIcon } from "../Icons";
+import {
+  DribbbleIcon,
+  GithubIcon,
+  LinkedinIcon,
+  SunIcon,
+  TwitterIcon,
+} from "../Icons";
 import siteMetadata from "../../utils/siteMetaData";
 import { useThemeSwitch } from "../Hooks/useThemeSwitch";
 
 const Header = () => {
+  const [mode, setMode] = useThemeSwitch();
+  const [click, setClick] = useState(false);
 
-  const [mode, setMode] = useThemeSwitch()
+  const toggle = () => {
+    setClick(!click);
+  };
 
   return (
-    <header className="w-full p-4 px-10 flex items-center justify-between">
+    <header className="w-full p-4 px:5 sm:px-10 flex items-center justify-between">
       <Logo />
-      <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50">
-        <Link href="/" className="mr-2">Home</Link>
-        <Link href="/about" className="mx-2">About</Link>
-        <Link href="/contact" className="ml-2 mx-2">Contact</Link>
+      <button className="inline-block sm:hidden z-50" onClick={toggle}>
+        <div className="w-6 cursor-pointer transition-all ease duration-300">
+          <div className="relative">
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                transform: click
+                  ? "rotate(-45deg) translateY(0)"
+                  : "rotate(0deg) translateY(6px)",
+              }}
+            >
+              &nbsp;
+            </span>
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                opacity: click ? 0 : 1,
+              }}
+            >
+              &nbsp;
+            </span>
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                transform: click
+                  ? "rotate(45deg) translateY(0)"
+                  : "rotate(0deg) translateY(-6px)",
+              }}
+            >
+              &nbsp;
+            </span>
+          </div>
+        </div>
+      </button>
+      <nav
+        className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize
+      items-center hidden sm:flex
+      fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50"
+      >
+        <Link href="/" className="mr-2">
+          Home
+        </Link>
+        <Link href="/about" className="mx-2">
+          About
+        </Link>
+        <Link href="/contact" className="ml-2 mx-2">
+          Contact
+        </Link>
         <button onClick={() => setMode(mode === "light" ? "dark" : "light")}>
-            <SunIcon />
+          <SunIcon />
         </button>
       </nav>
 
-      <div>
-        <a href={siteMetadata.linkedin} className="inline-block w-6 h-6 mr-4"><LinkedinIcon className="hover:scale-125 transition-all ease duration-200"/></a>
-        <a href={siteMetadata.twitter} className="inline-block w-6 h-6 mr-4"><TwitterIcon className="hover:scale-125 transition-all ease duration-200"/></a>
-        <a href={siteMetadata.github} className="inline-block w-6 h-6 mr-4"><GithubIcon className="hover:scale-125 transition-all ease duration-200"/></a>
-        <a href={siteMetadata.dribbble} className="inline-block w-6 h-6 mr-4"><DribbbleIcon className="hover:scale-125 transition-all ease duration-200"/></a>
+      <div className="hidden sm:flex items-center">
+        <a href={siteMetadata.linkedin} className="inline-block w-6 h-6 mr-4">
+          <LinkedinIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a href={siteMetadata.twitter} className="inline-block w-6 h-6 mr-4">
+          <TwitterIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a
+          href={siteMetadata.github}
+          className="inline-block w-6 h-6 mr-4 dark:fill-light "
+        >
+          <GithubIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a href={siteMetadata.dribbble} className="inline-block w-6 h-6 mr-4">
+          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
       </div>
     </header>
   );
